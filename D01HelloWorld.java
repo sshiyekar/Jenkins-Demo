815cb8767c74fbe7cace5b907a68bb2f11af9b0b
sushmita <sshiyekar@gmail.com>
2023-02-21 08:39:23 +0530

    adding and editing

--- D01HelloWorld.java
+++ D01HelloWorld.java
@@ -1,7 +1,7 @@
 public class D01HelloWorld {
 
 	public static void main(String[] args) {
-       System.out.println("Hello Wolrd through cmd maven with");
+       System.out.println("Hello Wolrd through cmd maven with notepad");
 	}
 
 }
